fc17e9649eb1cca0714588e75514d9885274af1f
Jppcamilo <jpcamilopereira@gmail.com>
2025-05-22 18:40:39 -0300

    Adicionei mais uma função no js

--- js/script.js
+++ js/script.js
@@ -12,6 +12,22 @@ function atualizarStatus() {
   document.getElementById('vida-gorila').textContent = vidaGorila;
   document.getElementById('humanos-restantes').textContent = humanos.filter(h => h).length;
   document.getElementById('ataques-feitos').textContent = ataquesFeitos;
-  }
 
-  
+
+  const divHumanos = document.getElementById('humanos');
+  divHumanos.innerHTML = '';
+  humanos.forEach((vivo, i) => {
+    const h = document.createElement('div');
+    h.className = 'humano' + (vivo ? '' : ' derrotado');
+    divHumanos.appendChild(h);
+  });
+
+  salvarEstado();
+  verificarFimDeJogo();
+}
+
+function salvarEstado() {
+  localStorage.setItem('vidaGorila', vidaGorila);
+  localStorage.setItem('humanos', JSON.stringify(humanos));
+  localStorage.setItem('ataquesFeitos', ataquesFeitos);
+}
