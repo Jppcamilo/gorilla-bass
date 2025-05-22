4a415e90305018518b659ce22d504d07f367b8ac
Jppcamilo <jpcamilopereira@gmail.com>
2025-05-22 18:28:32 -0300

    Adicionei um If e uma função no js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 let vidaGorila = 100;
 let humanos = Array(100).fill(true);
 let ataquesFeitos = 0;
+
+if(localStorage.getItem('vidaGorila')) {
+  vidaGorila = parseInt(localStorage.getItem('vidaGorila'));
+  humanos = JSON.parse(localStorage.getItem('humanos'));
+  ataquesFeitos = parseInt(localStorage.getItem('ataquesFeitos'));
+}
+
+function atualizarStatus() {
+  document.getElementById('vida-gorila').textContent = vidaGorila;
+  document.getElementById('humanos-restantes').textContent = humanos.filter(h => h).length;
+  document.getElementById('ataques-feitos').textContent = ataquesFeitos;
+  }
+
+  
